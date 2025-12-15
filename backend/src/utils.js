e20bf0d9bd5f9bc66909808e0bfa17e5d2f6fb84
ucenyp23 <ucenyp23@spstrutnov.cz>
2025-12-15 11:43:25 +0100

    none

--- backend/src/utils.js
+++
@@ -1,7 +0,0 @@
-import dayjs from "dayjs";
-export const toCelsius = (value, unit) => {
-  if (unit === "C") return Number(value);
-  return (Number(value) - 32) * 5 / 9;
-};
-export const toFahrenheit = (c) => (c * 9 / 5) + 32;
-export const isoNow = () => dayjs().toISOString();
--- frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-import { GraphQLClient, gql } from "graphql-request";
-const endpoint = "http://localhost:4000/graphql";
-const client = new GraphQLClient(endpoint, { credentials: "include" });
-
-export async function gqlRequest(query, variables) {
-  return client.request(query, variables);
-}
